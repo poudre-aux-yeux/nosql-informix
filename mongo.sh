0da675ab162e48869666c2fee5c8e1a81059cbcc
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-19 16:11:25 +0200

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,5 @@
+```sh
+mongoimport --db stores_demo --collection customer < customer.json
+mongoimport --db stores_demo --collection cust_calls < cust_calls.json
+mongoimport --db stores_demo --collection orders < orders.json
+```
