296a7bdf24a679ed50bf28d28e017fa7c6fa71e3
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-19 16:20:37 +0200

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,3 @@
-```sh
 mongoimport --db stores_demo --collection customer < customer.json
 mongoimport --db stores_demo --collection cust_calls < cust_calls.json
 mongoimport --db stores_demo --collection orders < orders.json
-```
