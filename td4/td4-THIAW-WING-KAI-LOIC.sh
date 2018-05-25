98941120ecb0e0cd2f95b1ef7f28cbb191272f4c
MushuLeDragon <22367990+MushuLeDragon@users.noreply.github.com>
2018-05-25 17:22:36 +0200

    Update td4-THIAW-WING-KAI-LOIC.sh

--- td4/td4-THIAW-WING-KAI-LOIC.sh
+++ td4/td4-THIAW-WING-KAI-LOIC.sh
@@ -8,24 +8,23 @@ echo "Convertir les instructions SQL Informix suivantes, en instruction find Mon
 mongo epsinosql --eval 'db.customer.createIndex( { company: "text" } )' >> td4-THIAW-WING-KAI-LOIC.log
 mongo epsinosql --eval 'db.customer.find({ $text: { $search: "Sports -town"} })' >> td4-THIAW-WING-KAI-LOIC.log
 mongo epsinosql --eval 'db.cust_calls.createIndex( { call_descr: "text" } )' >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval 'db.cust_calls.find({ $text: { $search: "hero watch tennis"} })' >> td4-THIAW-WING-KAI-LOIC.log
+mongo epsinosql --eval 'db.cust_calls.find({ $text: { $search: "\"hero watch tennis\""} })' >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 
 
 echo "Question 2" >> td4-THIAW-WING-KAI-LOIC.log
 echo "Corriger les instructions MongoDB suivantes afin qu’elles fonctionnent sur les collections : __CUSTOMER__ et __ORDERS__ afin de récupérer les documents dont les champs sont null ou vide" >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval '' >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval '' >> td4-THIAW-WING-KAI-LOIC.log
+mongo epsinosql --eval 'db.customer.find ({ address2: null })' >> td4-THIAW-WING-KAI-LOIC.log
+mongo epsinosql --eval 'db.orders.find( { $or: [ { paid_date : { $type: 11 }}, { paid_date: null }, { paid_date: { $exists: false } } ] } )' >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 
 
 echo "Question 3" >> td4-THIAW-WING-KAI-LOIC.log
 echo "Corriger les instructions suivantes afin qu’elles retournent seulement les champs _id , fname et lname__ dans la collection __CUSTOMER__ (ces requêtes retournent chacune un document)" >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval '' >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval '' >> td4-THIAW-WING-KAI-LOIC.log
-mongo epsinosql --eval '' >> td4-THIAW-WING-KAI-LOIC.log
+mongo epsinosql --eval 'db.customer.find( { state: "CA", fname: "Arnold" }, {fname: 1, lname: 1} )' >> td4-THIAW-WING-KAI-LOIC.log
+mongo epsinosql --eval 'db.customer.find( { zipcode: "85008"}, {fname: 1, lname: 1} )' >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 echo "------------------------------------------------------------------------------" >> td4-THIAW-WING-KAI-LOIC.log
 
